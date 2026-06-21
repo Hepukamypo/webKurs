4047487b4313b68855d99c4c413830a69e5bf544
Hepukamypo <vovasaush@gmail.com>
2026-06-22 01:34:37 +0300

    WebSocket для форумов откат

--- unbound/catalog/routing.py
+++ unbound/catalog/routing.py
@@ -2,6 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<other_id>\d+)/$',        consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/forum/(?P<thread_id>\d+)/$',      consumers.ForumConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<other_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
 ]
